1c98d0ce4077ec69d1ce98e009b61385a8051470
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-12 23:19:03 +0530

    lib

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 var exp=require('express')
 const app=exp();
 var rout=require("./router/bookrouter")
-var rout=require("./router/authorrt")
+var rrout=require("./router/authorrt")
 const path=require('path')
 app.set("view engine","ejs")
 app.set("views","./src/view")
 app.use(exp.static(path.join(__dirname,"/public")))
 //var app=new exp();
 
+
 app.use("/book",rout)
+
+app.use("/author",rrout)
 app.get("/",function(req,res){
  res.render('index',{nav:[{link:"/book",Title:"Book"},{link:'/author',Title:"Author"}],Pagetitle:"Library"})
 //res.sendFile(__dirname+"/src/view/index.html")
 })
-
+// app.use("/author",function(req,res){
+//     res.render("author",{nav:[{link:"/book",Title:"Book"},{link:'/author',Title:"Author"}],Pagetitle:"Library"})
+// })
 
 app.listen(3000,function(req,res){
     console.log("server is ready")
--- router/authorrt.js
+++ router/authorrt.js
@@ -1,3 +1,43 @@
-var ex=require('express')
-var auroot=ex.Router()
-module.ex=auroot;
+var ex=require('express');
+
+var auroot=ex.Router();
+module.exports=auroot;
+var arr_authors=[
+    {
+    name:"Chetan bhagat",
+    pic:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Chetan_Bhagat%2C_India.jpg/220px-Chetan_Bhagat%2C_India.jpg"
+
+},
+{
+    name:"A. P. J. Abdul Kalam ",
+    pic:"https://img.etimg.com/thumb/height-450,width-800,imgsize-210956,msid-48247272/.jpg"
+},
+{
+    name:"John Green",
+    pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpzv8dkJENqJc6fLhOYg0L7exANhq2tfBchiN-siX3RQND3hrf"
+},
+{
+    name:"J.K. Rowling",
+    pic:"https://image.cnbcfm.com/api/v1/image/104979551-GettyImages-623391110.jpg?v=1532563691&w=1400&h=950"
+},
+{
+    name:"Ruskin Bond",
+    pic:"https://images.outlookindia.com/public/uploads/gallery/20140522/ruskin_bond_20140602.jpg"
+},
+{
+    name:"Ravinder Singh",
+    pic:"https://pbs.twimg.com/profile_images/1122842034446856192/axKts38G.png"
+},
+{
+    name:"Anne Frank",
+    pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJIFp4YEVmlQRGirs7GmPpBnlOGO7Gs6GP4Lfm2HsimzuZ2Pd5"
+}
+]
+auroot.get("/",function(req,res){
+   console.log(arr_authors.length)
+    res.render("author",{nav:[{link:"/book",Title:"Book"},{link:'/author',Title:"Author"}],Pagetitle:"Authors",authrlist:arr_authors.sort()})
+})
+auroot.get("/:id",function(req,res){
+    var id=req.params.id
+    res.render("readmrau",{nav:[{link:"/book",Title:"Book"},{link:'/author',Title:"Author"}],Pagetitle:"Authors",authrlist:arr_authors[id]})
+})
